c12dc2b1b6298c9dc505e77bc0dd9e9bd3a4d80f
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-05-15 17:55:04 +0700

    feat: доделать логику работы кнопок и всех счетчиков в панели результатов

--- index.js
+++ index.js
@@ -4,14 +4,35 @@ const clearButton = document.querySelector('.clear-button')
 
 const rightAnswers = ['19', '40 minutes', 'balcony', 'tennis courts'];
 
-let studentsAnswers = document.querySelectorAll('.exercise__input')
+const studentsAnswers = document.querySelectorAll('.exercise__input')
+const resultPanel = document.querySelector('.exercise__result')
+let totalAnswers = document.querySelector('.total')
+let countOfRightAnswers = document.querySelector('.number')
+let percentOfRightAnswers = document.querySelector('.percent')
 
 checkButton.addEventListener('click', () => handleClickCheckButton())
 showButton.addEventListener('click', () => handleClickShowButton())
 clearButton.addEventListener('click', () => handleClickClearButton ())
 
 function handleClickCheckButton () {
-    takeAnswers()
+    let counterForAnswers = 0
+    for (let i=0; i<studentsAnswers.length; i++) {
+        studentsAnswers[i].classList.remove('exercise__input_correct')
+        studentsAnswers[i].classList.remove('exercise__input_wrong')
+    }
+    for (let i=0; i<studentsAnswers.length; i++) {
+        if (studentsAnswers[i].value === rightAnswers[i]) {
+            studentsAnswers[i].classList.add('exercise__input_correct')
+            counterForAnswers = counterForAnswers + 1
+        }
+        else {
+            studentsAnswers[i].classList.add('exercise__input_wrong')
+        }        
+    }
+    console.log(counterForAnswers)
+    resultPanel.classList.add('exercise__result_visible')
+    countOfRightAnswers.innerHTML = counterForAnswers
+    percentOfRightAnswers.innerHTML = ((counterForAnswers / rightAnswers.length) * 100);
 }
 
 function handleClickShowButton () {
@@ -24,12 +45,11 @@ function handleClickClearButton () {
     for (let i=0; i<studentsAnswers.length; i++) {
         studentsAnswers[i].value = ''
     }
+    for (let i=0; i<studentsAnswers.length; i++) {
+        studentsAnswers[i].classList.remove('exercise__input_correct')
+        studentsAnswers[i].classList.remove('exercise__input_wrong')
+    }
+    resultPanel.classList.remove('exercise__result_visible')
 }
 
-function takeAnswers () {
-    let studentsAnswersValues = [];
-    for (let i=0; i<studentsAnswers.length; i++) {
-        studentsAnswersValues.push(studentsAnswers[i].value)
-    }
-    return studentsAnswersValues
-}
+totalAnswers.innerHTML = rightAnswers.length
